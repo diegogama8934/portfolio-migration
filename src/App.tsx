4e2bf5168c2d4e8eee5881ba866e258034a9c346
Diego Martinez Garcia <diegogama8934@gmail.com>
2025-06-23 23:55:58 -0600

    ADD (packages): tanstack query setted up

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,25 @@ import { GlobalLayout } from "./ui/layout/GlobalLayout";
 import { Home } from "./ui/pages/Home";
 import { AboutMe } from "./ui/pages/AboutMe";
 import { Projects } from "./ui/pages/Projects";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
 
 function App() {
+  const queryClient = new QueryClient();
+
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<GlobalLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutMe" element={<AboutMe />} />
-          <Route path="/projects" element={<Projects />} />
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route element={<GlobalLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/aboutMe" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
