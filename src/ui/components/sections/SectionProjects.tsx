59ddb6a2c23f9d884d527280e031aec48612a7b7
Diego Martinez Garcia <diegogama8934@gmail.com>
2025-06-26 05:36:05 -0600

    FIX (projects): projects section redesigned

--- src/ui/components/sections/SectionProjects.tsx
+++ src/ui/components/sections/SectionProjects.tsx
@@ -1,37 +1,91 @@
-import { Link } from "react-router";
-import { ProjectResumeCard } from "../atoms/ProjectResumeCard";
-import { projectsResume } from "../../../constants";
+
+import { motion } from "motion/react";
 
 export function SectionProjects() {
   return (
-    <section className="relative flex flex-col gap-16 py-20">
-      <div className="relative z-10 flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <h2 className="section-title">Proyectos</h2>
-          <span className="text-3xl animate-bounce inline-block">🚀</span>
-        </div>
+    <div
+      id="projects"
+      className="relative z-10 flex flex-col gap-20"
+    >
+      <section className="relative flex flex-col gap-16">
 
-        <Link
-          className="group section-btn-link"
-          to="/projects"
+        <motion.h2 className="text-xl md:text-5xl font-semibold text-center text-gray-500 leading-relaxed mt-20"
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
         >
-          <span className="text-blue-700 font-bold lg:text-lg text-base group-hover:text-blue-800 transition-colors">
-            Ver más
-          </span>
-          <div className="section-btn-link-icon group-hover:scale-110">
-            <span className="material-symbols-outlined !text-xl">arrow_outward</span>
-          </div>
-        </Link>
-      </div>
+          Work that <span className="gradient-text font-extrabold">speaks for itself</span>.
+        </motion.h2>
 
-      <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {projectsResume.map(project => (
-          <ProjectResumeCard
-            key={project.name}
-            {...project}
-          />
-        ))}
-      </div>
-    </section>
+        <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 gap-8">
+
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center justify-center">
+                <span className="material-symbols-outlined !text-[200px] !text-gray-300">school</span>
+              </div>
+              <h3 className="mt-10 text-3xl font-bold gradient-text">TEACH</h3>
+              <p className="text-2xl mb-8 text-gray-500 leading-relaxed font-light max-w-2xl">
+                Virtual platform for teachers and students to create and manage their classes.
+              </p>
+              <motion.a
+                href="https://teach.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 flex items-center gap-2 w-fit border border-gray-200 rounded-md px-4 py-2"
+              >
+                Read project
+                <span className="material-symbols-outlined !text-base">arrow_outward</span>
+              </motion.a>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center justify-center">
+                <span className="material-symbols-outlined !text-[200px] !text-gray-300">wallet</span>
+              </div>
+              <h3 className="mt-10 text-3xl font-bold gradient-text">We plan</h3>
+              <p className="text-2xl mb-8 text-gray-500 leading-relaxed font-light max-w-2xl">
+                Platform for planning and managing personal, couple or family finances.
+              </p>
+              <motion.a
+                href="https://weplan.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 flex items-center gap-2 w-fit border border-gray-200 rounded-md px-4 py-2"
+              >
+                Read project
+                <span className="material-symbols-outlined !text-base">arrow_outward</span>
+              </motion.a>
+            </div>
+          </div>
+
+
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center justify-center">
+                <span className="material-symbols-outlined !text-[200px] !text-gray-300">work</span>
+              </div>
+              <h3 className="mt-10 text-3xl font-bold gradient-text">Bolsa de trabajo UAQ</h3>
+              <p className="text-2xl mb-8 text-gray-500 leading-relaxed font-light max-w-2xl">
+                Job board for the Autonomous University of Querétaro.
+              </p>
+              <motion.a
+                href="https://bolsa.uaq.mx"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 flex items-center gap-2 w-fit border border-gray-200 rounded-md px-4 py-2"
+              >
+                Read project
+                <span className="material-symbols-outlined !text-base">arrow_outward</span>
+              </motion.a>
+            </div>
+          </div>
+
+        </div>
+      </section >
+    </div >
   );
 }
