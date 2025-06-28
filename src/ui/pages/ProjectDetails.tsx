56d8c1d9fc6bc550c6cb8cdb9f526769a3cdd51e
Diego Martinez Garcia <diegogama8934@gmail.com>
2025-06-28 15:18:54 -0600

    ADD (single proj): start doing page

--- src/ui/pages/ProjectDetails.tsx
+++ src/ui/pages/ProjectDetails.tsx
@@ -1,6 +1,38 @@
 import { useParams } from "react-router";
+import { useTranslation } from "react-i18next";
 
 export function ProjectDetails() {
+  const { t } = useTranslation();
   const { id } = useParams();
-  return <div>ProjectDetails {id}</div>;
+
+  console.log(id);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-96">
+      <h1 className="section-title">TEACH</h1>
+      
+      <p className="section-description">
+        {t("projects.teachDescription")}
+      </p>
+      
+      <article className="flex gap-20 w-full mt-10">
+        <aside className="w-64">
+          <h2 className="section-description">Menu</h2>
+          <ul className="list-none pl-8 mt-8 flex flex-col gap-4">
+            <li className="w-full">
+              <a href="#problem" className="text-gray-400 w-full block hover:text-gray-600 text-lg">Problem</a>
+            </li>
+            <li className="w-full">
+              <a href="#solution" className="text-gray-400 w-full block hover:text-gray-600 text-lg">Solution</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="flex-1">
+          <h2 className="section-description !text-left" id="description">Problem</h2>
+        </main>
+      
+      </article>
+    
+    </div>
+  )
 }
